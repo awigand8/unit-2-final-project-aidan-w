6e3edc53cc347d43ff5e9c1d0ed7fb27525d9641
awigand8 <aidanconnorw@gmail.com>
2026-03-07 02:50:13 -0600

    initialized spring boot project and added config for database connection

---
+++ unit-2-final-project/src/main/java/com/example/unit_2_final_project/Unit2FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.unit_2_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2FinalProjectApplication.class, args);
+	}
+
+}
---
+++ unit-2-final-project/src/test/java/com/example/unit_2_final_project/Unit2FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit_2_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
