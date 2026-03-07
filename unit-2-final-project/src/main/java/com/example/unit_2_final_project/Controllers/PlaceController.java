d3f87c0a329b40a5012dc8206087057e059e3c99
awigand8 <aidanconnorw@gmail.com>
2026-03-07 05:09:24 -0600

    fixed PlaceController

--- unit-2-final-project/src/main/java/com/example/unit_2_final_project/Controllers/PlaceController.java
+++ unit-2-final-project/src/main/java/com/example/unit_2_final_project/Controllers/PlaceController.java
@@ -1,4 +1,23 @@
 package com.example.unit_2_final_project.Controllers;
 
+import com.example.unit_2_final_project.Models.Place;
+import com.example.unit_2_final_project.Repositories.PlaceRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/places")
 public class PlaceController {
+
+    private final PlaceRepository placeRepository;
+
+    public PlaceController(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    @GetMapping
+    public List<Place> getAllPlaces() {
+        return placeRepository.findAll();
+    }
 }
